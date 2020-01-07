059775d2ad7e2962c0ce78d7065fe2c261330e99
Jeremy Katoff <jkatoff@gmail.com>
2020-01-07 16:06:21 -0500

    ciommit

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers} from 'redux';
+import postsReducer from './postsReducer';
+import usersReducer from './usersReducer';
+
+export default  combineReducers({
+  posts: postsReducer,
+  users: usersReducer
+}); 
+
+  
+
+
